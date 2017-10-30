39421c9e31580ce6a60c73ffdb024feb268cfe2a
Robert Clayton <robbie.c.clayton@gmail.com>
2017-10-30 16:33:26 +0000

    debugging stack trace performed

--- spec/oyster_card_spec.rb
+++ spec/oyster_card_spec.rb
@@ -1,4 +1,10 @@
 
 describe Oystercard do
-  
+
+# NameError: uninitialized constant Oystercard
+# where: ./spec/oyster_card_spec.rb
+# line : 2
+# this means there is an undefined local variable or method
+# suggestion: require in the file where 'Oystercard' is
+
 end
